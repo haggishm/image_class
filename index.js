63e71b6fb40487c0e49cc2cc0e3d064f45a84fc6
haggishm <30947652+haggishm@users.noreply.github.com>
2019-08-28 18:54:08 +1200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const webcamElement = document.getElementById('webcam');
 const classifier = knnClassifier.create();
+//<link href = "style.css" rel = "stylesheet" />
 
 let net;
 
@@ -18,7 +19,6 @@ async function app() {
     // Get the intermediate activation of MobileNet 'conv_preds' and pass that
     // to the KNN classifier.
     const activation = net.infer(webcamElement, 'conv_preds');
-
     // Pass the intermediate activation to the classifier.
     classifier.addExample(activation, classId);
   };
@@ -27,22 +27,26 @@ async function app() {
   document.getElementById('class-a').addEventListener('click', () => addExample(0));
   document.getElementById('class-b').addEventListener('click', () => addExample(1));
   document.getElementById('class-c').addEventListener('click', () => addExample(2));
+  document.getElementById('class-na').addEventListener('click', () => addExample(3));
+ 
 
   while (true) {
     if (classifier.getNumClasses() > 0) {
       // Get the activation from mobilenet from the webcam.
       const activation = net.infer(webcamElement, 'conv_preds');
       // Get the most likely class and confidences from the classifier module.
-      const result = await classifier.predictClass(activation);
-      const classes = ['A', 'B', 'C'];
+      const result = await classifier.predictClass(activation, 10);
+      const classes = ['A', 'B', 'C', 'No_Action'];
+      document.getElementById("console").innerText.textAlign = "center"; 
       document.getElementById('console').innerText = `
-        prediction: ${classes[result.classIndex]}\n
-        probability: ${result.confidences[result.classIndex]}
+        prediction: ${classes[parseInt(result.label)]}\n
+        probability: ${result.confidences[parseInt(result.label)]}
       `;
-     }
-
+    }
     await tf.nextFrame();
+
   }
+  
 }
 
 async function setupWebcam() {
